1f6114e29ad40ad3aa924c37b753dd4021353ce1
fercholo0730 <fercholo0730@hotmail.com>
2021-11-08 21:29:18 -0500

    fixing the responsive dispaly

--- asincro.js
+++ asincro.js
@@ -1,10 +1,9 @@
 let consumo_api = (pagina = 1, busqueda = false) => {
   let contenedor_pagina = document.querySelector("#data_personajes");
+  let info_footer = document.querySelector("#info_footer");
   let character_search = document.getElementById("input_search").value;
   let button_pages = document.getElementById("button_pagesPN");
   let alive_color = "";
-
-  //   let url_api = "https://rickandmortyapi.com/api/character";
 
   if (!busqueda) {
     if (pagina == 1) {
@@ -35,46 +34,56 @@ let consumo_api = (pagina = 1, busqueda = false) => {
         }
         contenedor_pagina.innerHTML += `
             <div class="characters d-flex flex-row">
-            <div class="picture">
-            <img src=${data_personaje.image} alt="" class="product-img">
-            </div>
-            <div class="description col">
-            <div class="name">
-            <h3 class="links">
-            "${data_personaje.name}"
-            </h3>
-            </div>
-            <div class="live">
-            <div style="background-color: ${alive_color}; height: 12px; width: 12px; border-radius: 50%; margin-top: 3px; margin-right: 10px;"></div>
-            <h6>
-            ${data_personaje.status} - ${data_personaje.species}
-            </h6>
-            </div>
-            <div class="location mt-3">
-            <h5>
-            Last known location:
-            </h5>
-            </div>
-            <div class="locataion-resp">
-            <h4 class="links">
-            ${data_personaje.location.name}
-            </h4>
-            </div>
-            <div class="first-seen mt-3">
-            <h5>
-            Firts seen in:
-            </h5>
-            </div>
-            <div class="first-seen-resp">
-            <h4 class="links">
-            ${data_personaje.origin.name}
-            </h4>
-            </div>
-            </div>
+              <div class="picture">
+                <img src=${data_personaje.image} alt="" class="product-img">
+              </div>
+              <div class="description col">
+                <div class="name">
+                <a href="https://rickandmortyapi.com/api/character/${data_personaje.id}" style="text-decoration: none">
+                  <h3 class="links">
+                    ${data_personaje.name}
+                  </h3>
+                </a>
+                </div>
+                <div class="live">
+                  <div style="background-color: ${alive_color}; height: 12px; width: 12px; border-radius: 50%; margin-top: 3px; margin-right: 10px;"></div>
+                    <h6>${data_personaje.status} - ${data_personaje.species}</h6>
+                  </div>
+                <div class="location mt-3">
+                  <h5>
+                    Last known location:
+                  </h5>
+                </div>
+                <div class="locataion-resp">
+                  <a href="${data_personaje.location.url}" style="text-decoration: none">
+                  <h4 class="links">
+                    ${data_personaje.location.name}
+                  </h4></a>
+                </div>
+                <div class="first-seen mt-3">
+                  <h5>
+                    Firts seen in:
+                  </h5>
+                </div>
+                <div class="first-seen-resp">
+                  <a href="${data_personaje.origin.url}" style="text-decoration: none">
+                    <h4 class="links">
+                      ${data_personaje.origin.name}
+                    </h4>
+                  </a>
+                </div>
+              </div>
             </div>
             `;
       }
-      debugger;
+
+      info_footer.innerHTML = `
+        <div class="me-3">CHARACTERS: ${datico_pepe.info.count}</div>
+        <div class="me-3">LOCATIONS: 126</div>
+            <div class="">
+                EPISODES: 51
+            </div>
+      `;
       let previous_char = "";
       let next_char = "";
 
